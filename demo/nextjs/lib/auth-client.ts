32216c52904cb5a3854ab2c87507a587ac7b1faa
Allen Thich <allenthich@gmail.com>
2025-05-27 14:21:51 +0900

    clean(demo): remove client plugins

--- demo/nextjs/lib/auth-client.ts
+++ demo/nextjs/lib/auth-client.ts
@@ -1,40 +1,9 @@
 import { createAuthClient } from "better-auth/react";
-import {
-	organizationClient,
-	passkeyClient,
-	twoFactorClient,
-	adminClient,
-	multiSessionClient,
-	oneTapClient,
-	oidcClient,
-	genericOAuthClient,
-} from "better-auth/client/plugins";
+import {} from "better-auth/client/plugins";
 import { toast } from "sonner";
-import { stripeClient } from "@better-auth/stripe/client";
 
 export const client = createAuthClient({
-	plugins: [
-		organizationClient(),
-		twoFactorClient({
-			onTwoFactorRedirect() {
-				window.location.href = "/two-factor";
-			},
-		}),
-		passkeyClient(),
-		adminClient(),
-		multiSessionClient(),
-		oneTapClient({
-			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-			promptOptions: {
-				maxAttempts: 1,
-			},
-		}),
-		oidcClient(),
-		genericOAuthClient(),
-		stripeClient({
-			subscription: true,
-		}),
-	],
+	plugins: [],
 	fetchOptions: {
 		onError(e) {
 			if (e.error.status === 429) {
@@ -44,14 +13,4 @@ export const client = createAuthClient({
 	},
 });
 
-export const {
-	signUp,
-	signIn,
-	signOut,
-	useSession,
-	organization,
-	useListOrganizations,
-	useActiveOrganization,
-} = client;
-
-client.$store.listen("$sessionSignal", async () => {});
+export const {} = client;
