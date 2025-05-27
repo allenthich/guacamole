4dc529927abafa1d2715a8da387ce2df23f5c84f
Allen Thich <allenthich@gmail.com>
2025-05-27 13:30:08 +0900

    chore: disable cookie logic and adapter test

--- packages/better-auth/src/integrations/next-js.ts
+++ packages/better-auth/src/integrations/next-js.ts
@@ -1,5 +1,4 @@
 import type { BetterAuthPlugin } from "../types";
-import { parseSetCookieHeader } from "../cookies";
 import { createAuthMiddleware } from "../plugins";
 
 export function toNextJsHandler(
@@ -35,25 +34,25 @@ export const nextCookies = () => {
 						if (returned instanceof Headers) {
 							const setCookies = returned?.get("set-cookie");
 							if (!setCookies) return;
-							const parsed = parseSetCookieHeader(setCookies);
-							const { cookies } = await import("next/headers");
-							const cookieHelper = await cookies();
-							parsed.forEach((value, key) => {
-								if (!key) return;
-								const opts = {
-									sameSite: value.samesite,
-									secure: value.secure,
-									maxAge: value["max-age"],
-									httpOnly: value.httponly,
-									domain: value.domain,
-									path: value.path,
-								} as const;
-								try {
-									cookieHelper.set(key, decodeURIComponent(value.value), opts);
-								} catch (e) {
-									// this will fail if the cookie is being set on server component
-								}
-							});
+							// const parsed = parseSetCookieHeader(setCookies);
+							// const { cookies } = await import("next/headers");
+							// const cookieHelper = await cookies();
+							// parsed.forEach((value, key) => {
+							// 	if (!key) return;
+							// 	const opts = {
+							// 		sameSite: value.samesite,
+							// 		secure: value.secure,
+							// 		maxAge: value["max-age"],
+							// 		httpOnly: value.httponly,
+							// 		domain: value.domain,
+							// 		path: value.path,
+							// 	} as const;
+							// 	try {
+							// 		cookieHelper.set(key, decodeURIComponent(value.value), opts);
+							// 	} catch (e) {
+							// 		// this will fail if the cookie is being set on server component
+							// 	}
+							// });
 							return;
 						}
 					}),
