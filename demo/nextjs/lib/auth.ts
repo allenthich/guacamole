e8e5d9c1169b7bad100040d78d285b5150267e27
Allen Thich <allenthich@gmail.com>
2025-05-27 13:30:44 +0900

    chore(demo): remove auth plugin references

--- demo/nextjs/lib/auth.ts
+++ demo/nextjs/lib/auth.ts
@@ -1,16 +1,5 @@
 import { betterAuth } from "better-auth";
-import {
-	bearer,
-	admin,
-	multiSession,
-	organization,
-	twoFactor,
-	oneTap,
-	oAuthProxy,
-	openAPI,
-	oidcProvider,
-	customSession,
-} from "better-auth/plugins";
+import {} from "better-auth/plugins";
 import { reactInvitationEmail } from "./email/invitation";
 import { LibsqlDialect } from "@libsql/kysely-libsql";
 import { reactResetPasswordEmail } from "./email/reset-password";
@@ -18,9 +7,6 @@ import { resend } from "./email/resend";
 import { MysqlDialect } from "kysely";
 import { createPool } from "mysql2/promise";
 import { nextCookies } from "better-auth/next-js";
-import { passkey } from "better-auth/plugins/passkey";
-import { expo } from "@better-auth/expo";
-import { stripe } from "@better-auth/stripe";
 import { Stripe } from "stripe";
 
 const from = process.env.BETTER_AUTH_EMAIL || "delivered@resend.dev";
@@ -116,89 +102,6 @@ export const auth = betterAuth({
 			clientSecret: process.env.TWITTER_CLIENT_SECRET || "",
 		},
 	},
-	plugins: [
-		organization({
-			async sendInvitationEmail(data) {
-				await resend.emails.send({
-					from,
-					to: data.email,
-					subject: "You've been invited to join an organization",
-					react: reactInvitationEmail({
-						username: data.email,
-						invitedByUsername: data.inviter.user.name,
-						invitedByEmail: data.inviter.user.email,
-						teamName: data.organization.name,
-						inviteLink:
-							process.env.NODE_ENV === "development"
-								? `http://localhost:3000/accept-invitation/${data.id}`
-								: `${
-										process.env.BETTER_AUTH_URL ||
-										"https://demo.better-auth.com"
-									}/accept-invitation/${data.id}`,
-					}),
-				});
-			},
-		}),
-		twoFactor({
-			otpOptions: {
-				async sendOTP({ user, otp }) {
-					await resend.emails.send({
-						from,
-						to: user.email,
-						subject: "Your OTP",
-						html: `Your OTP is ${otp}`,
-					});
-				},
-			},
-		}),
-		passkey(),
-		openAPI(),
-		bearer(),
-		admin({
-			adminUserIds: ["EXD5zjob2SD6CBWcEQ6OpLRHcyoUbnaB"],
-		}),
-		multiSession(),
-		oAuthProxy(),
-		nextCookies(),
-		oidcProvider({
-			loginPage: "/sign-in",
-		}),
-		oneTap(),
-		customSession(async (session) => {
-			return {
-				...session,
-				user: {
-					...session.user,
-					dd: "test",
-				},
-			};
-		}),
-		stripe({
-			stripeClient: new Stripe(process.env.STRIPE_KEY || "sk_test_"),
-			stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET!,
-			subscription: {
-				enabled: true,
-				plans: [
-					{
-						name: "Starter",
-						priceId: STARTER_PRICE_ID.default,
-						annualDiscountPriceId: STARTER_PRICE_ID.annual,
-						freeTrial: {
-							days: 7,
-						},
-					},
-					{
-						name: "Professional",
-						priceId: PROFESSION_PRICE_ID.default,
-						annualDiscountPriceId: PROFESSION_PRICE_ID.annual,
-					},
-					{
-						name: "Enterprise",
-					},
-				],
-			},
-		}),
-		expo(),
-	],
+	plugins: [],
 	trustedOrigins: ["exp://"],
 });
